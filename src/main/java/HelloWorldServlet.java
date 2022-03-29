47e1bb024ab9295d2a7321314e2bf3f4d9ace58f
Mina McGonigal <mcgonigal.mina@gmail.com>
2022-03-29 13:47:54 -0500

    servlet trial

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -11,6 +11,8 @@ public class HelloWorldServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
         res.setContentType("text/html");
         PrintWriter out = res.getWriter();
-        out.println("<h2>Hello, Mina</h2>");
+        out.println("<h2>Hello, Mina</h2>" +
+                "<h4>this is first servlet</h4>" +
+                "");
     }
 }
---
+++ src/main/java/hello2Servlet.java
@@ -0,0 +1,17 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "hello2", urlPatterns = "/hellohi")
+public class hello2Servlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException{
+        res.setContentType("text/html");
+        PrintWriter out =res.getWriter();
+        out.println("<h1>Hello this is second servlet I created. </h1>" +
+                "<p>안녕하세요 ㅋㅋㅋㅋ 완전 대박 완전 신기 로컬 호스트 </p>");
+    }
+}
