08f4cc9d09b98b31eb3e3e444160880af2eb4c90
Mina McGonigal <mcgonigal.mina@gmail.com>
2022-03-30 10:47:20 -0500

    servlet practice

--- src/main/java/hello2Servlet.java
+++ src/main/java/hello2Servlet.java
@@ -5,14 +5,17 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "hello2", urlPatterns = "/hellohi")
+@WebServlet(name = "hello2", urlPatterns = "/hello2")
 public class hello2Servlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException{
         res.setContentType("text/html");
         PrintWriter out =res.getWriter();
-        out.println("<h1>Hello this is second servlet I created. </h1>" +
-                "<p>안녕하세요 ㅋㅋㅋㅋ 완전 대박 완전 신기 로컬 호스트 </p>" +
-                "<p>완전 신기함 </p>");
+        String outputRes = "<h1> hello , world ! <h1>";
+        if(req.getParameter("name") !=null ){
+           outputRes = "Hello, " + req.getParameter("name") + " !! ";
+        }
+        out.println(outputRes
+                );
     }
 }
