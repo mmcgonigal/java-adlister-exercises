99e6a204b099c3ca561a305880b0d971c881c643
Mina McGonigal <mcgonigal.mina@gmail.com>
2022-04-08 10:27:32 -0500

    update

--- src/main/java/MySQLAdsDao.java
+++ src/main/java/MySQLAdsDao.java
@@ -1,8 +1,24 @@
-import com.sun.jdi.connect.spi.Connection;
 
+import java.sql.*;
+import java.sql.Connection;
+import java.util.ArrayList;
 import java.util.List;
+import com.mysql.cj.jdbc.Driver;
 
 public class MySQLAdsDao implements Ads {
+private Connection connection;
+public MySQLAdsDao(Config config){
+    try{
+        DriverManager.registerDriver(new Driver());
+        this.connection = DriverManager.getConnection(
+                config.getUrl(),
+                config.getUser(),
+                config.getPassword()
+        );
+    }catch(SQLException e){
+        e.printStackTrace();
+    }
+}
 
 
 
@@ -10,11 +26,42 @@ public class MySQLAdsDao implements Ads {
 
     @Override
     public List<Ad> all() {
-        return null;
+        List<Ad> ads = new ArrayList<>();
+        String query = "SELECT * ads";
+        try{
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery(query);
+         while(rs.next()){
+             Ad ad = new Ad(
+                     rs.getLong("id"),
+                     rs.getLong("user_id"),
+                     rs.getString("title"),
+                     rs.getString("description")
+
+             );
+             ads.add(ad);
+         }
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        return ads;
     }
 
     @Override
     public Long insert(Ad ad) {
-        return null;
+        Long lastInsertedId = 0L;
+        try{
+           Statement statement = connection.createStatement();
+           String insertQuery = String.format("INSERT INTO ads(user_id,title,description) VALUES(%d,'%s','%s')",ad.getUserId(),ad.getTitle(),ad.getDescription());
+           statement.executeUpdate(insertQuery,Statement.RETURN_GENERATED_KEYS);
+           ResultSet rs = statement.getGeneratedKeys();
+           rs.next();
+           lastInsertedId =rs.getLong(1);
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return lastInsertedId;
+
     }
 }
---
+++ src/main/java/seeder.sql
@@ -0,0 +1,8 @@
+USE adlister_db;
+INSERT INTO users(username, email, password) VALUES ('mina-m','mina@mina.com','mina86');
+
+INSERT INTO ads(user_id, title, description) VALUES
+    ('1234','testing','this is the testads'),
+    ('1234','testing','this is the testads'),
+    ('1234','testing','this is the testads'),
+    ('1234','testing','this is the testads');
